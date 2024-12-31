c6fbf1217408881c11fdd38a54a0e9746e3a0f41
adpc2102 <andrespaz2002@gmail.com>
2024-12-31 17:17:52 -0400

    Deploy 4.0

--- project/Back/app.py
+++ project/Back/app.py
@@ -302,5 +302,4 @@ def update_supervisor(department_name):
     return jsonify({"message": "Supervisor actualizado"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+app.run(host='0.0.0.0', port=5000)
